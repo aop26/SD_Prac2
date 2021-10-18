5d218b9e9365e83e44d3107f346c9b584c0b6621
a <a@a.a>
2021-10-18 17:26:48 +0200

    updated waitingtimeserver

--- FWQ_WaitingTimeServer.py
+++ FWQ_WaitingTimeServer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+from os import close
 import sys
 import socket
 import customutils as cu
 import atexit
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, consumer
 
 #Lectura y comprobación de argumentos
 cu.uso = "FWQ_WaitingTimeServer [Puerto de escucha] [ip:puerto(gestor de colas)]"
@@ -28,7 +29,18 @@ s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((cu.getIP(),puerto))
 print("Created server at "+cu.getIP()+" with port "+str(puerto))
-atexit.register(lambda : s.close())
+s.listen()
 sensorReader = KafkaConsumer('sensors',bootstrap_servers=sys.argv[2])
+def exit_handler():
+    s.close() 
+    sensorReader.close()
+atexit.register(exit_handler)
 while True:
+    (clientSocket, clientIP) = s.accept()
+    msgs = ""
+    for msg in consumer:
+        print(msg)
+        msgs.append(msg+"-")
+    clientSocket.send(msgs)
+    clientSocket.close()
     
