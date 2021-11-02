c863bc662a23d65936f98fbfaa61bf9b090e8286
Andrés Ortiz Perona <aop26@gcloud.ua.es>
2021-11-02 16:06:31 +0100

    Added db functions to cu

--- customutils.py
+++ customutils.py
@@ -3,7 +3,12 @@ import re
 import sys 
 import time
 import kafka
+from os.path import exists
+import os
+import sqlite3
 uso = ""
+
+nexit = True
 
 def printUso():
     print("Uso del programa:")
@@ -39,17 +44,54 @@ def checkIP(ip2check, que):
     return (ip,puerto)
 
 def kp(ip):
-    while True:
+    while nexit:
         try:
             return kafka.KafkaProducer(bootstrap_servers=ip)
         except:
             print("No se ha podido conectar a kafka. Reintentando en 1 segundo.")
             time.sleep(1)
 
-def kc(ip):
-    while True:
+def kc(ip, topic):
+    global nexit
+    while nexit:
         try:
-            return kafka.KafkaConsumer(bootstrap_servers=ip)
-        except:
-            print("No se ha podido conectar a kafka. Reintentando en 1 segundo.")
+            print(ip)
+            return kafka.KafkaConsumer(topic,bootstrap_servers=ip)
+        except Exception as e:
+            print("No se ha podido conectar a kafka. Reintentando en 1 segundo.\n",e)
             time.sleep(1)
+            if(not nexit):
+                quit()
+
+def openDB():
+    if(not exists('database.db')):
+        con = sqlite3.connect('database.db')
+        with open('scriptBD', 'r') as file:
+            script = file.read()
+        scriptlines = script.split(';')
+        cur = con.cursor()
+        for line in scriptlines:
+            print("ejecutando ",line+';')
+            try:
+                cur.execute(line+';')
+                con.commit()
+            except Exception as e:
+                print("Error creating db",e)
+                os.remove("database.db")
+                con.close()
+                quit()
+        return con
+    else:
+        return sqlite3.connect('database.db')
+
+def leerAtr(ID):
+    con = openDB()
+    cur = con.cursor()
+    cur.execute(f"select x, y, waitTime,maxPeople from RIDE where ID = {ID}")
+    res = cur.fetchone()
+    con.close()
+    return [res[0],res[1]], res[2], res[3]
+
+def stopAll():
+    global nexit
+    nexit = False
