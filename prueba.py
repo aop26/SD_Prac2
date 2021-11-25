d74bb2b66cbe2400c966c4a3527f0745316c124d
Andres <aop26@gcloud.ua.es>
2021-11-25 17:30:36 +0100

    mejorado sistema de login de engine

--- prueba.py
+++
@@ -1,8 +0,0 @@
-import customutils as cu
-con = cu.openDB()
-cur = con.cursor()
-ID = 1
-cur.execute(f"select x, y, waitTime,maxPeople from RIDE where ID = {ID};")
-res = cur.fetchone()
-con.close()
-print([res[0],res[1]],res[2],res[3])
