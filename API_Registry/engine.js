3be6174fb9b8b7e0f33f22057dc73d96ce540c67
Andres Ortiz <aop26@gcloud.ua.es>
2021-12-29 14:17:51 +0100

    engine api finished

--- API_Registry/engine.js
+++ API_Registry/engine.js
@@ -1,91 +1,83 @@
 const https = require("https");
 const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
-const journey = require('journey');
+const app = require('express')();
 //const app = express();
-var mrouter = new (journey.Router)();
-
-const options ={
-	key: fs.readFileSync('cert/key.pem'),
-	cert: fs.readFileSync('cert/cert.pem')
+//var mrouter = new (journey.Router)();
+function readMap(){
+    return fs.readFileSync('../map.txt','utf8')
 }
-
+function isNumeric(s) {
+    return !isNaN(s - parseFloat(s));
+}
 function strToMap(str) {
-    function isNumeric(s) {
-        return !isNaN(s - parseFloat(s));
-    }
     var m = Array(20).fill(0).map(x => Array(20).fill(0));
+    var currentCount = 0;
     for (let i = 0; i < str.length; i++) {
         if(str[i]=='r'){
             var auxStr = "";
             i++;
             while(i<str.length && isNumeric(str[i])){
-                auxStr+=mapStr[i];
+                auxStr+=str[i];
                 i++;
             }
             i--;
-            m[currentCount-(currentCount/20)*20][currentCount/20]=auxStr;
+            m[currentCount-parseInt(currentCount/20)*20][parseInt(currentCount/20)]=auxStr;
         }else if(str[i]=='u'){
             var auxStr = "";
             i++;
             while(i< str.length && isNumeric(str[i])){
-                auxStr+=mapStr[i];
+                auxStr+=str[i];
                 i+=1;
             }
             i--;
-            m[currentCount-(currentCount/20)*20][currentCount/20]="../img/"+auxStr;
+            m[currentCount-parseInt(currentCount/20)*20][parseInt(currentCount/20)]="/img/"+auxStr;
         }
-        
+        currentCount++;
     }
+    //console.log(str, m)
     return m
 }
+function mapHtml(host){
+    var start = '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1"><style>th {border: 1px solid black;padding:0}th{width:50px}th{height:50px}</style></head><body><table>';
+    var end = '</table></body></html>'
+    var body = ''
+    m = strToMap(readMap());
+    for (let i = 0; i < m.length; i++) {
+        body+='<tr>'
+        for (let j = 0; j < m[i].length; j++) {
+            body+='<th>'
+            var element = m[j][i];
+            //console.log(element)
+            if(element!=0){
+                if(isNumeric(element)){
+                    body+=element;
+                }else{
+                    body+='<img src="https://'+host+element+'" alt="'+element+'" width="50" height="50">';
+                }
+            }
+            body+='</th>'
+        }
+        body+='</tr>'
+        
+    }
+    return start + body + end;
+}
 
+const options ={
+	key: fs.readFileSync('cert/key.pem'),
+	cert: fs.readFileSync('cert/cert.pem')
+}
+	
 // Se define el puerto
-const port=443;
-mrouter.map(function(){
+const port=3001;
 
-	this.root.bind(function (req,res){
-		res.send("<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>")
-	})
-
-	// GET lee un usr y devuelve el hash de la contraseña
-	// https://localhost:3000/usr/mlb51
-	this.get(/^usr\/([A-Za-z0-9_]+)$/).bind(function (request, response, usr) {
-
-		let db = new sqlite3.Database('/home/miquel/Escritorio/SD_Prac2/database.db', sqlite3.OPEN_READWRITE, (err) => {
-			if (err) {
-			  console.error(err.message);
-			} else{
-				console.log('Connected to the database.');
-			}
-		});
-
-		console.log("Llamada a GET para: "+usr);
-
-		db.serialize(() => {
-			db.each(`SELECT password FROM CLIENT where username= '${usr}'`, (err, row) => {
-			  if (err) {
-				console.error(err.message);
-				response.send(err.message);
-			  }
-			  response.send(row.password);
-			});
-		});
-
-		db.close()
-	});
-
-
+app.get('/',function(req,res) {
+    res.send(mapHtml(req.get('host')))
+})
+app.get('/img/:img', function(req,res){
+    var path = require('path')
+    res.sendFile(path.resolve(__dirname+"/../img/"+req.params['img']))
 })
 
-https.createServer(options, function(request, response){
-	var body = "";
 
-	request.addListener('data', function(chunk){body+=chunk});
-	request.addListener('end', function(){
-		mrouter.handle(request, body, function(result){
-			response.writeHead(result.status, result.headers);
-			response.end(result.body);
-		})
-	})
-}).listen(port);
+https.createServer(options, app).listen(port);
